fb1b6dd762b76c36f8646da7a8e0eb5415c12a4c
kangfarih <kangfarih@users.noreply.github.com>
2021-02-15 07:21:18 +0000

    deploy: dc0e0b274febabe64463c4148366ca14d3973ace

---
+++ _next/static/Hmp8DWwJOqmCzr-UUr2nu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-424c3f17d5df0f81c19c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8a5236cfc3f34e5c777a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
